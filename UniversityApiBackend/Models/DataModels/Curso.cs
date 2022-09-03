c05b23c7f4ff7284d850b5fca6a7099509aad673
ivan3911 <ivan3911@hotmail.com>
2022-09-02 20:34:07 -0500

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public enum level
+    {
+        essencial = 0,
+        intermediate,
+        advanced
+    }
+    public class Curso : BaseEntity
+    {
+
+        [Required, StringLength(int.MaxValue)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetAudiences { get; set; } = string.Empty;
+
+        [Required]
+        public string Goals { set; get; } = string.Empty;
+
+        [Required]
+        public string Requirements { set; get; } = string.Empty;
+
+        [Required]
+        public level Level { set; get; }
+    }
+}
